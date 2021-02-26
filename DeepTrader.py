8447786bd0a76dce8026b3727f8b676ec1a2ccb1
Brendon Eby <brendoneby2021@u.northwestern.edu>
2021-02-25 23:03:57 -0600

    Added progressbar

--- DeepTrader.py
+++ DeepTrader.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from progressbar import progressbar
+import progressbar
 from torch.utils.data import DataLoader
 import matplotlib as plt
 
