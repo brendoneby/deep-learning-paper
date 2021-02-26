32757038cd2555cecfae87599bb56c62af14baea
Brendon Eby <brendoneby2021@u.northwestern.edu>
2021-02-26 10:43:10 -0600

    trying 6e-5

--- datasets.py
+++ datasets.py
@@ -14,10 +14,10 @@ def noramlize_training_data(data):
     data[:,1] = np.sign(data[:,13] - data[:,6])
     max_vals = np.max(data,axis=0)
     min_vals = np.min(data,axis=0)
-    print(max_vals)
-    print(min_vals)
-    # norm_data = (data-min_vals)/(max_vals-min_vals)
-    # np.savetxt("data/snapshots_normalized.csv", norm_data, delimiter=",")
+    # print(max_vals)
+    # print(min_vals)
+    norm_data = (data-min_vals)/(max_vals-min_vals)
+    np.savetxt("data/snapshots_normalized.csv", norm_data, delimiter=",")
 
 # Only for Data preparation
 def merge_csv_files():
@@ -118,8 +118,8 @@ def build_dataloader(fn, batch_size, device) -> DataLoader:
     return dataset
     # return DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False)
 
-data = load_data("data/snapshots.csv", "train")
-#
-noramlize_training_data(data)
+# data = load_data("data/snapshots.csv", "train")
+# #
+# noramlize_training_data(data)
 
 # merge_csv_files()
